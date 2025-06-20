17367984bdcb0b3cf519755bf5ab5e0f2f27e448
Simon <simonvanstaalduine@home.nl>
2025-06-20 14:54:28 +0200

    added xy folder naming

--- flow_analysis_comps/visualizing/video.py
+++ flow_analysis_comps/visualizing/video.py
@@ -4,6 +4,19 @@ from flow_analysis_comps.io.video import videoIO
 import dask.array as da
 
 
+def coord_to_folder(x, y, precision=2):
+    def fmt(val):
+        val = round(val, precision)
+        if val < 0:
+            prefix = "n"
+            val = -val
+        else:
+            prefix = ""
+        return prefix + str(val).replace(".", "_")
+
+    return f"x_{fmt(x)}_y_{fmt(y)}"
+
+
 class VideoVisualizer:
     def __init__(self, video_path):
         self.video_info = videoIO(video_path)
@@ -13,8 +26,12 @@ class VideoVisualizer:
     def save_mp4_video(self, separate_into_positions=False):
         video_array = self.array.compute()
 
+        folder_name = coord_to_folder(
+            self.metadata.position.x, self.metadata.position.y, precision=3
+        )
+
         if separate_into_positions:
-            output_path = Path(self.metadata.storage_path)/ "video" / f"{self.metadata.position.x:.1}_{self.metadata.position.y:.1}" 
+            output_path = Path(self.metadata.storage_path) / "video" / folder_name
             output_path.mkdir(parents=True, exist_ok=True)
         else:
             output_path = Path(self.metadata.storage_path)
