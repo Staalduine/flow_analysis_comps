0a90a3ba78bb32de18f8c3162decfdb40178b7f5
Simon <simonvanstaalduine@home.nl>
2025-06-20 16:21:47 +0200

    dsds

--- flow_analysis_comps/visualizing/video.py
+++ flow_analysis_comps/visualizing/video.py
@@ -32,9 +32,9 @@ class VideoVisualizer:
         )
 
         timestamp_str = self.metadata.date_time
-        timestamp_obj = datetime.fromisoformat(timestamp_str)
+        # timestamp_obj = datetime.fromisoformat(timestamp_str)
         timeformat = "%Y%m%d_%H%M%S"
-        formatted_timestamp = timestamp_obj.strftime(timeformat)
+        formatted_timestamp = timestamp_str.strftime(timeformat)
 
         if separate_into_positions:
             output_path = Path(self.metadata.storage_path) / "video" / folder_name
