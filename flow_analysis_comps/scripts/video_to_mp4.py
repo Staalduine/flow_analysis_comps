2fe30ef667c2ec37a215da8316f9796ba21c1fe1
Simon <simonvanstaalduine@home.nl>
2025-06-20 14:34:55 +0200

    added changed output path

--- flow_analysis_comps/scripts/video_to_mp4.py
+++ flow_analysis_comps/scripts/video_to_mp4.py
@@ -1,4 +1,5 @@
 from flow_analysis_comps.visualizing.video import VideoVisualizer
+import os
 
 
 def process(run_info_index, process_args=None):
@@ -13,10 +14,22 @@ def process(run_info_index, process_args=None):
     None
     """
     row = run_info_index
+    separate_into_positions = process_args[0] if process_args else False
     if "total_path" not in row:
         raise KeyError("The key 'total_path' is missing in the provided run_info_index dictionary.")
     path = row["total_path"]
+    rename_json_to_video_metadata(path)
 
     vizi = VideoVisualizer(path)
-    vizi.save_mp4_video()
+    vizi.save_mp4_video(separate_into_positions)
     return
+
+
+def rename_json_to_video_metadata(path):
+    for file in os.listdir(path):
+        if file.endswith(".json"):
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path, "video_metadata.json")
+            os.rename(old_path, new_path)
+            print(f"Renamed {old_path} to {new_path}")
+            break
--- flow_analysis_comps/visualizing/video.py
+++ flow_analysis_comps/visualizing/video.py
@@ -10,10 +10,17 @@ class VideoVisualizer:
         self.metadata = self.video_info.metadata
         self.array: da.Array = self.video_info.video_array
 
-    def save_mp4_video(self):
+    def save_mp4_video(self, separate_into_positions=False):
         video_array = self.array.compute()
+
+        if separate_into_positions:
+            output_path = Path(self.metadata.storage_path)/ "video" / f"{self.metadata.position.x:.1}_{self.metadata.position.y:.1}" 
+            output_path.mkdir(parents=True, exist_ok=True)
+        else:
+            output_path = Path(self.metadata.storage_path)
+
         writer = imageio.get_writer(
-            Path(self.metadata.storage_path) / "Video.mp4",
+            output_path / "Video.mp4",
             fps=int(self.metadata.camera_settings.frame_rate),
             codec="libx264",
         )
