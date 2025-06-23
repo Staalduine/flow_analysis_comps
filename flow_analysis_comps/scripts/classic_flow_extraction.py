9d201416f1c137b0db324f5961e30ae4f2119b60
Simon <simonvanstaalduine@home.nl>
2025-06-23 14:53:33 +0200

    dsds

--- flow_analysis_comps/scripts/classic_flow_extraction.py
+++ flow_analysis_comps/scripts/classic_flow_extraction.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from flow_analysis_comps.io.video import videoIO
 from flow_analysis_comps.data_structs.kymographs import (
     GSTConfig,
     graphExtractConfig,
@@ -14,18 +14,31 @@ from flow_analysis_comps.visualizing.GraphVisualize import (
 )
 from flow_analysis_comps.processing.GSTSpeedExtract.extract_velocity import kymoAnalyser
 from flow_analysis_comps.processing.kymographing.kymographer import KymographExtractor
+from flow_analysis_comps.util.video_io import coord_to_folder
 
 
 def process(run_info_index, process_args):
     # expecting a float in um/s
-    speed_limit = process_args[0]
+    speed_limit = float(process_args[0])
+    separate_positions = bool(process_args[1]) if len(process_args) > 1 else False
     speed_config = GSTConfig(speed_limit=speed_limit)
 
     row = run_info_index
     path = Path(row["total_path"])
 
-    out_folder = path / "flow_analysis"
-    out_folder.mkdir(exist_ok=True)
+    video_io = videoIO(path)
+    pos_x, pos_y = video_io.metadata.position.x, video_io.metadata.position.y
+    video_position = coord_to_folder(pos_x, pos_y, precision=3)
+    
+    timeformat = "%Y%m%d_%H%M%S"
+    formatted_timestamp = video_io.metadata.date_time.strftime(timeformat)
+
+    if separate_positions:
+        out_folder: Path = path / "flow_analysis" / video_position / formatted_timestamp
+    else:
+        out_folder: Path = path / "flow_analysis" / formatted_timestamp
+
+    out_folder.mkdir(exist_ok=True, parents=True)
 
     graph_data = VideoGraphExtractor(path, graphExtractConfig()).edge_data
     kymograph_list = KymographExtractor(
