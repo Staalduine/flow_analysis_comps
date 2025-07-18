7e09215b10d77e2b2894565e1cfe87bff33eefa6
Simon <simonvanstaalduine@home.nl>
2025-07-18 13:59:14 +0200

    dsdsds

--- flow_analysis_comps/scripts/classic_flow_extraction.py
+++ flow_analysis_comps/scripts/classic_flow_extraction.py
@@ -57,14 +57,16 @@ def process_video(
     root_folder: Path,
     out_folder: Path,
     speed_config: GSTConfig,
-    kymo_extract_config: kymoExtractConfig = kymoExtractConfig(),
+    kymo_extract_config: kymoExtractConfig | None = None,
     video_position: str | None = None,
     formatted_timestamp: str | None = None,
     user_metadata: videoInfo | None = None,
 ):
     video_process_logger = setup_logger(name="flow_analysis_comps.video_processing")
     out_folder.mkdir(exist_ok=True, parents=True)
-
+    
+    if kymo_extract_config is None:
+        kymo_extract_config = kymoExtractConfig()
     if video_position is None:
         video_position = "vid"
     if formatted_timestamp is None:
