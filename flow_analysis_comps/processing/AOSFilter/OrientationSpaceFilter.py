added image padding added filter normalization fixed more axis problems fixed order updating added mirror padding function added testing cells to run_AOS

flow_analysis_comps/processing/AOSFilter/OrientationSpaceFilter.py
@@ -35,7 +35,12 @@ class OrientationSpaceFilter:
             self.params.space_frequency_width,
             coords,
         )
-        return angular_filter * radial_filter[:, :, None]
+
+        out_filter = angular_filter * radial_filter[:, :, None]
+
+        filter_sums = abs(np.sum(out_filter, axis=(0, 1)))
+        out_filter /= filter_sums
+        return out_filter
 
 
 def calculate_angular_filter(
@@ -65,7 +70,7 @@ def calculate_angular_filter(
             theta_s = theta_coords / s_a
 
             angularFilter = 2 * np.exp(-(theta_s**2) / 2)
-            angularFilter_reversed = angularFilter[::-1, ::-1, :]
+            angularFilter_reversed = np.fft.ifftshift(np.fft.fftshift(angularFilter)[::-1, ::-1])
             filterKernel = 0.5 * (angularFilter + angularFilter_reversed)
             filterKernel = filterKernel * (1 + 1j * (posMask * 2 - 1))
             return filterKernel

flow_analysis_comps/processing/Fourier/NLMSPrecise.py
@@ -12,7 +12,7 @@ def nlms_precise(
     mask=None,
     offset_angle=None,
     angle_multiplier=3,
-)-> np.ndarray:
+) -> np.ndarray:
     """Perform nonlocal maximum suppression. Sample for each point along its strongest angle, then remove all points that are not a maximum.
 
     Args:
@@ -29,7 +29,7 @@ def nlms_precise(
         ValueError: Raises if dimensionality is weird
 
     Returns:
-        np.ndarray: Array with masked (nan) pixels, suppressed (default: 0) pixels and non-suppressed pixels which carry the response value of the filter. 
+        np.ndarray: Array with masked (nan) pixels, suppressed (default: 0) pixels and non-suppressed pixels which carry the response value of the filter.
     """
     # Adjust to dimensionality
     match len(theta_max.shape):
@@ -51,9 +51,9 @@ def nlms_precise(
     # Set up size of nlms arrays
     rot_response_size = response.shape
 
-    ny = rot_response_size[0]
-    nx = rot_response_size[1]
-    na = rot_response_size[2]
+    ny = rot_response_size[1]
+    nx = rot_response_size[2]
+    na = rot_response_size[0]
     period = na * angle_multiplier
 
     # Pad data with more periods
@@ -63,7 +63,7 @@ def nlms_precise(
             assert isinstance(new_response, np.ndarray)
             response = new_response
     else:
-        response = np.moveaxis(response, 2, 0)
+        # response = np.moveaxis(response, 2, 0)
         rotationResponseTemp = resample(response[:, mask], period, axis=0)
         response = np.zeros((period, mask.shape[0], mask.shape[1]))
         response[:, :, :] = np.nan

flow_analysis_comps/processing/Fourier/OrientationSpaceManager.py
@@ -22,6 +22,7 @@ import numpy.typing as npt
 from flow_analysis_comps.processing.Fourier.NLMSPrecise import nlms_precise
 from flow_analysis_comps.processing.Fourier.findAllMaxima import interpft_extrema_fast
 from flow_analysis_comps.util.coord_space_util import wraparoundN
+from flow_analysis_comps.util.image_manips import mirror_pad_with_exponential_fade
 
 
 class orientationSpaceManager:
@@ -44,10 +45,15 @@ class orientationSpaceManager:
             instance of object
         """
         self.filter_params = params
-        self.image = image
+        self.image = (
+            image
+            if self.filter_params.padding <= 0
+            else mirror_pad_with_exponential_fade(image, self.filter_params.padding)
+        )
+
         self.os_filter: OrientationSpaceFilter = OrientationSpaceFilter(params)
-        self.filter_arrays, self.setup_imdims = self.init_filter_on_img(image)
-        self.response = self.get_response(image)
+        self.filter_arrays, self.setup_imdims = self.init_filter_on_img(self.image)
+        self.response = self.get_response(self.image, self.filter_params.padding)
         self.thresh_method = thresh_method
         self.mask = self.response.nlms_mask(thresh_method=thresh_method)
 
@@ -59,10 +65,13 @@ class orientationSpaceManager:
         """
         img_shape = img.shape
         filter_arrays = self.os_filter.calculate_numerical_filter(img_shape)
+        # print(filter_arrays.shape)
+
+        # print(abs(np.sum(filter_arrays, axis=(0, 1))))
         filter_shape = img_shape
         return filter_arrays, filter_shape
 
-    def get_response(self, img: np.ndarray):
+    def get_response(self, img: np.ndarray, pad: int):
         """Applies filter onto input image, first uses image shape to create filter arrays
 
         Args:
@@ -77,7 +86,10 @@ class orientationSpaceManager:
         # Apply filters
         ridge_resp = self.apply_ridge_filter(image_fft)
         edge_resp = self.apply_edge_filter(image_fft)
-        ang_resp = ridge_resp + edge_resp
+        ang_resp = ridge_resp + edge_resp # Output array, for now still (x, y, n)
+        if pad > 0:
+            # Crop out the padded region
+            ang_resp = ang_resp[pad:-pad, pad:-pad, ...]  # Handles 2D and 3D arrays
 
         # Create response object capable of further processing results
         return OrientationSpaceResponse(ang_resp)
@@ -135,7 +147,9 @@ class orientationSpaceManager:
             else:
                 f_hat = np.array([1])
 
-            f_hat = np.broadcast_to(f_hat.reshape(-1, 1,1), self.response.response_stack_fft.shape)
+            f_hat = np.broadcast_to(
+                f_hat.reshape(-1, 1, 1), self.response.response_stack_fft.shape
+            )
             # a_hat: np.ndarray = fftpack.fft(self.response.response_stack.real, axis=2)
 
             a_hat = self.response.response_stack_fft * f_hat
@@ -175,7 +189,7 @@ class orientationSpaceManager:
         return maximum_single_angle
 
     def get_all_angles(self) -> dict:
-        a_hat = self.response.response_stack_fft # dims = (D, x, y)
+        a_hat = self.response.response_stack_fft  # dims = (D, x, y)
         interpolated_extrema_dict = interpft_extrema_fast(a_hat, dim=0)
         return interpolated_extrema_dict
 
@@ -262,8 +276,11 @@ class orientationSpaceManager:
         maxima_highest = np.moveaxis(maxima_highest, 0, -1)
         return maxima_highest
 
-    def nlms_simple_case(self, order=5):
-        updated_response, filter = self.update_response_at_order_FT(order)
+    def nlms_simple_case(self, order: int | None = None):
+        if order:
+            updated_response, filter = self.update_response_at_order_FT(order)
+        else:
+            updated_response = self.response
         maximum_single_angle = self.get_max_angles()
         nlms_single = nlms_precise(
             updated_response.response_stack.real,

flow_analysis_comps/processing/Fourier/OrientationSpaceResponse.py
@@ -31,10 +31,11 @@ class OrientationSpaceResponse:
         """
         self.response_stack = response_array
         self.response_stack_fft: np.ndarray = fftpack.fft(self.response_stack.real, axis=2)
-        # Bring the response stack fft in line with this stuff (last axis to first axis)
+        # Bring the response stack fft in line with what the functions expect(n, x, y)
         self.response_stack_fft = np.moveaxis(self.response_stack_fft, -1, 0)
-        self.number_of_angles = self.response_stack.shape[-1]
-        self.range_of_angles = np.arange(self.response_stack.shape[-1]) / self.response_stack.shape[-1] * np.pi
+        self.response_stack = np.moveaxis(self.response_stack, -1, 0)
+        self.number_of_angles = self.response_stack.shape[0]
+        self.range_of_angles = np.arange(self.response_stack.shape[0]) / self.response_stack.shape[0] * np.pi
 
     def get_resp(self):
         return self.response_stack

flow_analysis_comps/processing/Fourier/findAllMaxima.py
@@ -62,7 +62,10 @@ def interpft_extrema_fast(
     response_fft_derivative1 = fftshift(response_fft_derivative1, axes=0)
     response_fft_deriv1_flat = response_fft_derivative1.reshape((response_fft_derivative1.shape[0], -1))
 
-"""Recode below to the actual method"""
+    real_response_angles = np.array([])
+    real_map = True
+
+    """Recode below to the actual method"""
     # # Compute coefficients for polynomial roots
     # coefficients = [response_fft_deriv1_flat[:, i] for i in range(response_fft_deriv1_flat.shape[1])]
     # roots_out = list(Parallel(n_jobs=n_jobs)(